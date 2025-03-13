f2278691126e32bb9b4db428feefbf35f2be6f6e
VictorCastro3 <viccas03@ucm.es>
2025-03-13 14:51:28 +0100

    Movimiento del personaje + gravedad + cambio de dirección con choque

---
+++ LRDdAelG/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 1;
+
+
+    private int direction = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position += new Vector3(speed * Time.deltaTime * direction, -1, 0);
+    }
+
+    void FixelUpdate()
+    {
+        Rigidbody2D rb;
+        
+    }
+
+    public void changeDirection()
+    {
+        direction *= -1;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision != null)
+        {
+            changeDirection();
+            Debug.Log("Cambia dirección");
+        }
+    }
+}
