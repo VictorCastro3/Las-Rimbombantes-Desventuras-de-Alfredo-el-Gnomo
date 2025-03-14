491ff7e9cd23b3a39d471b130283fd2f4f723a75
Ribir13 <victoa04@ucm.es>
2025-03-14 09:37:08 +0100

    player jump

--- LRDdAelG/Assets/Scripts/PlayerMovement.cs
+++ LRDdAelG/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 1;
-
+    [SerializeField] float springJump = 10f;
 
     private int direction = 1;
     // Start is called before the first frame update
@@ -25,13 +25,16 @@ public class PlayerMovement : MonoBehaviour
         gameObject.transform.rotation *= Quaternion.Euler(0, 180, 0);
         direction *= -1;
     }
-
+    public void JumpSpring()
+    {
+        gameObject.transform.position += new Vector3(0, springJump, 0);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision != null)
         {
             changeDirection();
-            Debug.Log("Cambia dirección");
+            Debug.Log("Cambia direcciï¿½n");
         }
     }
 }
