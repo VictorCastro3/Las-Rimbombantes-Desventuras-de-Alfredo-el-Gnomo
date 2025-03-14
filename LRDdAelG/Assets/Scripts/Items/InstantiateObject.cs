521ce414902ff9a198bba7dadc525d8cfddbb991
VictorCastro3 <viccas03@ucm.es>
2025-03-14 20:40:51 +0100

    Tileset sin terminar Y EL PUTO PROBLEMA DE LOS COJONES

--- LRDdAelG/Assets/Scripts/Items/InstantiateObject.cs
+++ LRDdAelG/Assets/Scripts/Items/InstantiateObject.cs
@@ -8,12 +8,11 @@ using UnityEngine;
 // Añadir aquí el resto de directivas using
 
 
-    #ROOTNAMESPACEBEGIN#
 /// <summary>
 /// Antes de cada class, descripción de qué es y para qué sirve,
 /// usando todas las líneas que sean necesarias.
 /// </summary>
-public class #SCRIPTNAME# : MonoBehaviour
+public class InstantiateObject : MonoBehaviour
 {
     // ---- ATRIBUTOS DEL INSPECTOR ----
     #region Atributos del Inspector (serialized fields)
@@ -24,7 +23,7 @@ public class #SCRIPTNAME# : MonoBehaviour
     // Ejemplo: MaxHealthPoints
 
     #endregion
-    
+
     // ---- ATRIBUTOS PRIVADOS ----
     #region Atributos Privados (private fields)
     // Documentar cada atributo que aparece aquí.
@@ -35,21 +34,21 @@ public class #SCRIPTNAME# : MonoBehaviour
     // Ejemplo: _maxHealthPoints
 
     #endregion
-    
+
     // ---- MÉTODOS DE MONOBEHAVIOUR ----
     #region Métodos de MonoBehaviour
-    
+
     // Por defecto están los típicos (Update y Start) pero:
     // - Hay que añadir todos los que sean necesarios
     // - Hay que borrar los que no se usen 
-    
+
     /// <summary>
     /// Start is called on the frame when a script is enabled just before 
     /// any of the Update methods are called the first time.
     /// </summary>
     void Start()
     {
-        #NOTRIM#
+
     }
 
     /// <summary>
@@ -57,20 +56,20 @@ public class #SCRIPTNAME# : MonoBehaviour
     /// </summary>
     void Update()
     {
-        #NOTRIM#
+
     }
     #endregion
 
     // ---- MÉTODOS PÚBLICOS ----
     #region Métodos públicos
-    // Documentar cada método que aparece aquí con ///<summary>
-    // El convenio de nombres de Unity recomienda que estos métodos
-    // se nombren en formato PascalCase (palabras con primera letra
-    // mayúscula, incluida la primera letra)
-    // Ejemplo: GetPlayerController
+    public GameObject Instantiate(GameObject prefab)
+    {
+        Debug.Log("a")
+        return Instantiate(prefab);
+    }
 
     #endregion
-    
+
     // ---- MÉTODOS PRIVADOS ----
     #region Métodos Privados
     // Documentar cada método que aparece aquí
@@ -78,8 +77,7 @@ public class #SCRIPTNAME# : MonoBehaviour
     // se nombren en formato PascalCase (palabras con primera letra
     // mayúscula, incluida la primera letra)
 
-    #endregion   
+    #endregion
 
-} // class #SCRIPTNAME# 
-#NOTRIM#
-#ROOTNAMESPACEEND# // namespace
+} // class InstantiateObject 
+// namespace
