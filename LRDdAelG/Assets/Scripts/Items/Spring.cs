1738cbeeced2bc8ede26746d4db3d7842f2588da
Ribir13 <victoa04@ucm.es>
2025-03-16 00:54:32 +0100

    Springs

--- LRDdAelG/Assets/Scripts/Items/Spring.cs
+++ LRDdAelG/Assets/Scripts/Items/Spring.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Spring : MonoBehaviour
 {
+    private Animator animator; 
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         PlayerMovement player = GameManager.Instance.GetPlayer().GetComponent<PlayerMovement>();
         if (player != null)
         {
             player.JumpSpring();
+            animator.SetTrigger("activated");
         }
     }
 }
