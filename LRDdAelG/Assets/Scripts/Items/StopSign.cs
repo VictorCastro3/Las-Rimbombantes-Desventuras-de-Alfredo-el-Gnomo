18896343ae4144b499a2d2850ccdbf62eed888c4
Ribir13 <victoa04@ucm.es>
2025-03-15 23:47:10 +0100

    Animaciones de la señal

--- LRDdAelG/Assets/Scripts/Items/StopSign.cs
+++ LRDdAelG/Assets/Scripts/Items/StopSign.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 
 public class StopSign : MonoBehaviour
 {
+    private Animator animator; 
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
     [SerializeField]private bool active = true;
     void OnMouseDown()
     {
+        if (active)
+        {
+            animator.SetInteger("state", 0);
+        }
+        else
+        {
+            animator.SetInteger("state", 1);
+        }
         active = !active;
         if (!active)
         {
