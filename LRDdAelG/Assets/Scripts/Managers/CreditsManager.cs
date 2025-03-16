e9180aa7d239c43ba84f4bb1155673e46dad1d0e
Ribir13 <victoa04@ucm.es>
2025-03-16 18:51:08 +0100

    Creditos

---
+++ LRDdAelG/Assets/Scripts/Managers/CreditsManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CreditsManager : MonoBehaviour
+{
+    public void RAAAH()
+    {
+        SceneManager.LoadScene("MENU");
+    }
+}
