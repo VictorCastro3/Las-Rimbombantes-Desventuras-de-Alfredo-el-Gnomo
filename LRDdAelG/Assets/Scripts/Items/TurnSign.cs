6c369a35fda570f1a84e17fb5f97809015fa2418
Ribir13 <victoa04@ucm.es>
2025-03-16 15:54:42 +0100

    Nivel nuevo + basura canon arreglada?

--- LRDdAelG/Assets/Scripts/Items/TurnSign.cs
+++ LRDdAelG/Assets/Scripts/Items/TurnSign.cs
@@ -7,15 +7,32 @@ using UnityEngine;
 public class TurnSign : MonoBehaviour
 {
     private bool turn = false;
+    private bool activatetime = false;
+    private float timer = 0f;
     private Animator animator; 
     void Awake()
     {
         animator = GetComponent<Animator>();
     }
+    void Update()
+    {
+        if (activatetime){
+            timer += Time.deltaTime;
+            if (timer >= 0.4f)
+            {
+                timer = 0f;
+                activatetime = false;
+            }
+        }
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         PlayerMovement player = GameManager.Instance.GetPlayer().GetComponent<PlayerMovement>();
-        if (player != null && !turn)
+        if (player != null && !turn && !activatetime)
+        {
+            player.changeDirection();
+            activatetime = true;
+        }
         {
             player.changeDirection();
         }
